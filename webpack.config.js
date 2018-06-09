404751c37bbf55df9e69938248f9b7c939a4b495
yhlben <yinhengliben@gmail.com>
2018-06-09 17:34:19 +0800

    更新webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path');
-const { VueLoaderPlugin } = require('vue-loader')
+const {
+  VueLoaderPlugin
+} = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
 
-module.exports = {
-  mode:'development',
+const isDev = process.env.NODE_ENV === 'development';
+
+const config = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     path: path.join(__dirname, 'dist')
   },
-  module:{
-    rules:[
-      {
+  module: {
+    rules: [{
         test: /\.vue$/,
         loader: 'vue-loader'
       },
@@ -29,17 +33,37 @@ module.exports = {
         ]
       },
       {
-        test:/\.(png|svg|jpg|jpeg)$/,
-        use:[
-          {
-            loader:'url-loader',
-            options:{
-              limit:1024,
-            }
+        test: /\.(png|svg|jpg|jpeg)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
           }
-        ]
+        }]
       }
     ],
   },
-  plugins: [new VueLoaderPlugin()]
+  plugins: [
+    new VueLoaderPlugin(), 
+    new HtmlWebpackPlugin(),
+  ]
 }
+
+if (isDev) {
+  config.mode = 'development';
+  config.devServer = {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9001,
+    hot:true,
+  };
+  // 热启动配置
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new webpack.DefinePlugin({
+    isDev: true,
+  }));
+  
+}
+
+
+module.exports = config;
